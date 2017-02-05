8b222d19d90e6949b3af346ec46aa6f645d76131
Andrew Panasyuk <avpk77@gmail.com>
2017-02-05 18:50:44 +0200

    one check - one method Totall: 8 checks

--- Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
+++ Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
@@ -7,19 +7,46 @@ import org.junit.Test;
 import com.andrewpanasyuk.anagrams.StringReverser;
 
 public class TestStringReverser {
+	StringReverser stringReverser = new StringReverser();
 
 	@Test
-	public void testReverse() {
-		StringReverser stringReverser = new StringReverser();
+	public void testReverse1() {
 		assertEquals("dcba", stringReverser.reverse("abcd"));
+	}
+
+	@Test
+	public void testReverse2() {
 		assertEquals("dcba dcBa", stringReverser.reverse("abcd aBcd"));
+	}
+
+	@Test
+	public void testReverse3() {
 		assertEquals("dcb*a", stringReverser.reverse("abc*d"));
+	}
+
+	@Test
+	public void testReverse4() {
 		assertEquals("dcb123*a,", stringReverser.reverse("abc123*d,"));
+	}
+
+	@Test
+	public void testReverse5() {
 		assertEquals("d1cba", stringReverser.reverse("a1bcd"));
+	}
+
+	@Test
+	public void testReverse6() {
 		assertEquals("123456", stringReverser.reverse("123456"));
+	}
+
+	@Test
+	public void testReverse7() {
 		assertEquals("/*-+", stringReverser.reverse("/*-+"));
+	}
+
+	@Test
+	public void testReverse8() {
 		assertEquals("abcABC123/*-+", stringReverser.reverse("CBAcba123/*-+"));
-		
 	}
 
 }
