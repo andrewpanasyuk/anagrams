1133d95329f47c9d31a5178e24d029d91c0569e2
Andrew Panasyuk <avpk77@gmail.com>
2017-02-05 15:38:18 +0200

    del check on cyryllic

--- Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
+++ Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
@@ -16,10 +16,8 @@ public class TestStringReverser {
 		assertEquals("dcb*a", stringReverser.reverse("abc*d"));
 		assertEquals("dcb123*a,", stringReverser.reverse("abc123*d,"));
 		assertEquals("d1cba", stringReverser.reverse("a1bcd"));
-		assertEquals("הדגבא", stringReverser.reverse("אבגדה"));
 		assertEquals("123456", stringReverser.reverse("123456"));
 		assertEquals("/*-+", stringReverser.reverse("/*-+"));
-		assertEquals("ְֱֲגבא123CBAcba/*-+", stringReverser.reverse("abcABC123אבגְֱֲ/*-+"));
 		assertEquals("abcABC123/*-+", stringReverser.reverse("CBAcba123/*-+"));
 		
 	}
