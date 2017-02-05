9e3a4ad2a6aafb3569e9a31b4ba2a33355589dad
Andrew Panasyuk <avpk77@gmail.com>
2017-02-05 12:24:16 +0200

    rename method

--- Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
+++ Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
@@ -9,7 +9,7 @@ import com.andrewpanasyuk.anagrams.StringReverser;
 public class TestStringReverser {
 
 	@Test
-	public void test() {
+	public void testReverse() {
 		StringReverser stringReverser = new StringReverser();
 		String rezult = stringReverser.reverse("abcd abc*d a1bcd");
 		assertEquals(rezult, "dcba dcb*a d1cba");
