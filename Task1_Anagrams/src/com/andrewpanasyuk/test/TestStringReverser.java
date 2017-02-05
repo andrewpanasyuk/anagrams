bae03aa319d9b705e4fa6f0b1956650f126e652f
Andrew Panasyuk <avpk77@gmail.com>
2017-02-05 14:33:52 +0200

    added a check for 9 types string: - only latin letter; - string with space; - letters and a special symbol; - letters, a special symbol, numbers; - letters and a number; - cyrillic letters; - only special symbols; - only numbers; - all symbol types.

--- Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
+++ Task1_Anagrams/src/com/andrewpanasyuk/test/TestStringReverser.java
@@ -11,8 +11,16 @@ public class TestStringReverser {
 	@Test
 	public void testReverse() {
 		StringReverser stringReverser = new StringReverser();
-		String rezult = stringReverser.reverse("abcd abc*d a1bcd");
-		assertEquals(rezult, "dcba dcb*a d1cba");
+		assertEquals("dcba", stringReverser.reverse("abcd"));
+		assertEquals("dcba dcBa", stringReverser.reverse("abcd aBcd"));
+		assertEquals("dcb*a", stringReverser.reverse("abc*d"));
+		assertEquals("dcb123*a,", stringReverser.reverse("abc123*d,"));
+		assertEquals("d1cba", stringReverser.reverse("a1bcd"));
+		assertEquals("הדגבא", stringReverser.reverse("אבגדה"));
+		assertEquals("123456", stringReverser.reverse("123456"));
+		assertEquals("/*-+", stringReverser.reverse("/*-+"));
+		assertEquals("ְֱֲגבא123CBAcba/*-+", stringReverser.reverse("abcABC123אבגְֱֲ/*-+"));
+		
 	}
 
 }
